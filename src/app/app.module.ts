b3cbc96bb7fcec467e05fcbae0fcb9947ffa6b68
JoeLee <karocksjoelee@gmail.com>
2017-09-05 17:50:21 +0800

    [Angular] Added Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { RealTimeService } from './realtime.service';
@@ -24,6 +25,7 @@ const routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
   providers: [RealTimeService],
